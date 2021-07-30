01a7f464f7b39abf321379adacb21a5fd89cdedd
Viacheslav Volkov <vexell@gmail.com>
2021-07-30 18:22:56 +0100

    Add new eslint

--- base.eslintrc.js
+++ base.eslintrc.js
@@ -20,7 +20,14 @@ module.exports = {
         semi: ['error', 'always'],
         'comma-dangle': 'off',
         quotes: ['error', 'single', { allowTemplateLiterals: true }],
-        'space-before-function-paren': ['error', 'never'],
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'always',
+                named: 'never',
+                asyncArrow: 'always',
+            },
+        ],
     },
     overrides: [
         {
